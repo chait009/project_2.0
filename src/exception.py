aa21a0a327324801780cd65f08acdf209e3bc003
chait009 <chaitanyach0511@gmail.com>
2025-03-19 09:50:04 -0500

    EDA AND Problem

--- src/exception.py
+++ src/exception.py
@@ -4,7 +4,7 @@ def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error cocured in python script name [{0}] line number [{1}] error message [{2}]".format()
-    file_name,exc-tb.tb_lineno,str(error)
+    file_name,exc_tb.tb_lineno,str(error)
     
     return error_message
     )
